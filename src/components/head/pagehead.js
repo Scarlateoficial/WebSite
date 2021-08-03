9f652aa7c3229179cdbaa7d2d06c808be24975ec
Julio <jn5.guape@gmail.com>
2021-08-03 15:19:28 -0300

    meta tag no head

--- src/components/head/pagehead.js
+++ src/components/head/pagehead.js
@@ -3,8 +3,16 @@ import Head from 'next/head'
 export default function PageHead({title}) {
     return (
         <Head>
-            <title>Alfa Tech | {title}</title>
             <link rel="icon" href="/favicon.ico" />
+
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#000000" />
+            <meta name="description" content="Alfa Tech"/>
+            <meta name="language" content="PT"/>
+            <meta name="author" content="Julio Nunes Avelar"/>
+            <meta name="url" content="https://website-alfa.vercel.app/"/>
+
+            <title>Alfa Tech | {title}</title>
         </Head>
     )
 }
